99d3187874452f6822a35c3f0cdf9dc20aaf8f59
Jt Miclat <jtmiclat@gmail.com>
2021-03-03 16:44:50 +0800

    Population Tooltip

--- src/components/tooltip/populationTooltip.js
+++ src/components/tooltip/populationTooltip.js
@@ -1,28 +1,65 @@
-import React from 'react';
 import { renderToString } from 'react-dom/server';
+import { Box, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core';
+const useStyles = makeStyles((theme) => ({
+  root: {},
+  value: {
+    fontWeight: 'bold',
+    fontSize: '24px',
+  },
+  section: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  marketShareWrapper: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  marketShareContainer: {
+    flexDirection: 'column',
+    display: 'flex',
+    padding: 2,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  marketShareValue: {
+    fontWeight: 'bold',
+    fontSize: '16px',
+  },
+}));
 
-const renderPopulationTooltip = (feature) => {
-  return renderToString(
-    <div>
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'space-between',
-        }}
-      >
-        <strong style={{ marginRight: '5px' }}>Total Population:</strong>
-        <span>{feature.properties.total_population.toFixed(2)}</span>
-      </div>
-      <div>
-        <strong style={{ marginRight: '5px' }}>Percent Women:</strong>
-        <span>20%</span>
-      </div>
-      <div>
-        <strong style={{ marginRight: '5px' }}>Percent Men:</strong>
-        <span>80%</span>
-      </div>
-    </div>
+const PopulationTooltip = ({ feature }) => {
+  const classes = useStyles();
+  return (
+    <Box className={classes.root}>
+      <Box className={classes.section}>
+        <Typography className={classes.value}>
+          {feature.properties.total_population.toFixed(0)}
+        </Typography>
+        <Typography variant='subtitle2'>Total Population</Typography>
+      </Box>
+      <Box className={classes.marketShareWrapper}>
+        <Box className={classes.marketShareContainer}>
+          <Typography className={classes.marketShareValue}>
+            {feature.properties.companyA_population.toFixed(0)}
+          </Typography>
+          <Typography variant='subtitle2'>Company A</Typography>
+        </Box>
+        <Box className={classes.marketShareContainer}>
+          <Typography className={classes.marketShareValue}>
+            {feature.properties.companyB_population.toFixed(0)}
+          </Typography>
+          <Typography variant='subtitle2'>Company B</Typography>
+        </Box>
+      </Box>
+    </Box>
   );
 };
+const renderPopulationTooltip = (feature) =>
+  renderToString(<PopulationTooltip feature={feature} />);
 
 export default renderPopulationTooltip;
