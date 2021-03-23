3a12f9978c9a362d193e290415b030bb359ab36a
Borja Muñoz <bmunoz@carto.com>
2021-03-23 09:53:49 +0100

    Remove Experian dataset scope

--- src/config/initialStateSlice.js
+++ src/config/initialStateSlice.js
@@ -26,7 +26,6 @@ export const oauthInitialState = {
       'dataservices:geocoding', // to use geocoding through Data Services API
       'dataservices:isolines', // to launch isochrones or isodistances through Data Services API
       'datasets:r:developers-admin.bangkok_opencellid_2019',
-      'datasets:r:developers-admin.th_grid_data_sample',
     ],
     authorizeEndPoint: 'https://carto.com/oauth2/authorize', // only valid if keeping https://localhost:3000/oauthCallback
   },
