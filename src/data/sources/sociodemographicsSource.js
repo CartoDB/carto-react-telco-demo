6e247c93f411592461b4ba84a3730fce0ff9a13d
Borja Muñoz <bmunoz@carto.com>
2021-03-23 12:17:00 +0100

    Use the tileset without null values

--- src/data/sources/sociodemographicsSource.js
+++ src/data/sources/sociodemographicsSource.js
@@ -2,7 +2,7 @@ const SOCIODEMOGRAPHICS_SOURCE_ID = 'sociodemographicsSource';
 
 const source = {
   id: SOCIODEMOGRAPHICS_SOURCE_ID,
-  data: `cartodb-on-gcp-pm-team.demo.th_grid_sample_data_tileset_nulls`,
+  data: `cartodb-on-gcp-pm-team.demo.th_grid_sample_data_tileset`,
   type: 'bigquery',
 };
 
